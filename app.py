639413c0a02e5ddfc653020618563f6eab6f0edb
hannah2014 <hxb303@student.bham.ac.uk>
2024-11-24 18:31:11 +0000

    add (very) basic PyQt5 GUI

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
 
-
+class Gui(QMainWindow):
+    def __init__(self):
+        super(Gui, self).__init__()
+        uic.loadUi("main.ui", self)
+        self.show()
 
 def main():
-    print("hello, world!")
+    app = QApplication([])
+    window = Gui()
+    app.exec_()
 
 if __name__ == '__main__':
     main()
