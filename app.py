0e432a0ea881c751b20d980cb9fd696be87af0b7
hannah2014 <hxb303@student.bham.ac.uk>
2024-11-30 02:33:54 +0000

    Implement basic UI: - add Qt-Fluent-Widgets as a dependency - remove main.ui (because UI mostly made of reusable cards easier to hand-code than use QTDesigner) - add MarkerCard class and populate a list of markers with dummy data

--- app.py
+++ app.py
@@ -1,15 +1,41 @@
 from PyQt5.QtWidgets import *
-from PyQt5 import uic
+from qfluentwidgets import *
 
-class Gui(QMainWindow):
-    def __init__(self):
-        super(Gui, self).__init__()
-        uic.loadUi("main.ui", self)
-        self.show()
+class MarkerCard(ElevatedCardWidget): # https://qfluentwidgets.com/pages/components/cardwidget
+
+    def __init__(self, markerName: str, numTestsMarked: str, rank: str, parent=None):
+        super().__init__(parent)
+        self.markerName = BodyLabel("Name: " + markerName, self)
+        self.numTestsMarked = BodyLabel("Tests marked: " + numTestsMarked, self)
+        self.rank = BodyLabel("Rank: " + rank, self)
+        self.trust = CheckBox("Trust")
+
+        self.vBoxLayout = QVBoxLayout(self)
+        self.vBoxLayout.setSpacing(10)
+
+        self.vBoxLayout.addWidget(self.markerName)
+        self.vBoxLayout.addWidget(self.numTestsMarked)
+        self.vBoxLayout.addWidget(self.rank)
+        self.vBoxLayout.addWidget(self.trust)
+
+        self.setFixedSize(400, 120)
 
 def main():
     app = QApplication([])
-    window = Gui()
+    window = QWidget()
+    windowLayout = QVBoxLayout()
+    window.setLayout(windowLayout)
+
+    windowLayout.addWidget(PushButton("Load Database"))
+    windowLayout.addWidget(PushButton("Connect to CSRS"))
+
+    windowLayout.addWidget(MarkerCard("Bob Teacher", "69", "Module lead"))
+    windowLayout.addWidget(MarkerCard("Bob Dylan", "70", "Module staff"))
+    windowLayout.addWidget(MarkerCard("Bob Marley", "71", "Module staff"))
+    windowLayout.addWidget(MarkerCard("Bob Ross", "72", "Module staff"))
+    windowLayout.addWidget(MarkerCard("Bob Odenkirk", "73", "Module staff"))
+
+    window.show()
     app.exec_()
 
 if __name__ == '__main__':
